1f9b464a515be1e047ac8ce0376661a4d69d8f2e
raulvazpe01~ <raul.vazquez.96@gmail.com>
2022-07-12 18:02:07 +0200

    formulario y validaciones

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,39 +1,57 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.bolsadeideas.springboot.form.app.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario") //Para mantener los datos aunque no los envíe por el form (como el identificador)
 public class FormController {
 
 	
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 	model.addAttribute("titulo","Formulario envío de usuarios");
+	usuario.setNombre("Jose");
+	usuario.setApellidos("Jemez");
+	usuario.setIdentificador("45454654Y");
+	model.addAttribute("usuario", usuario);
 		return "form";
 	}
 
 	
 	@PostMapping("/form")
-	public String procesar(Model model,
-			@RequestParam(name="username") String username,
-			@RequestParam(name="email")String email,
-			@RequestParam(name="password")String password) {
+	public String procesar(@Valid Usuario usuario,BindingResult result,Model model) {
 			
-	Usuario usuario1 = new Usuario();
-	usuario1.setEmail(email);
-	usuario1.setUsername(username);
-	usuario1.setPassword(password);
+		model.addAttribute("titulo", "Resultado del envío");
 	
-	model.addAttribute("titulo", "Resultado del envío");
+		if(result.hasErrors()) {
+
+			/*	NO HACE FALTA YA QUE TIMELEAF LO MANEJA DESDE LA VISTA	
+		Map<String, String>errores = new HashMap<>();
+		result.getFieldErrors().forEach(err->{ //obtenemos la lista de errores y la recorremos
+			//personalizamos el error obteniendo el nombre de la fila que ha fallado y personalizamos el mensaje
+			errores.put(err.getField(),"El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
+		});
+		
+		//enviamos el error para mostrarlo por pantalla
+		model.addAttribute("error", errores);   */
+
+		return "form";
+		}
 	
-	model.addAttribute("usuario1", usuario1);
+
+	
+	model.addAttribute("usuario", usuario);
 
 		return "resultado";
-	}
-}
+	}}
--- src/main/java/com/bolsadeideas/springboot/form/app/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/domain/Usuario.java
@@ -1,9 +1,19 @@
 package com.bolsadeideas.springboot.form.app.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
 
+	private String identificador;
+	@NotEmpty
+	private String nombre;
+	@NotEmpty
+	private String apellidos;
+	@NotEmpty
 	private String username;
+	@NotEmpty
 	private String password;
+	@NotEmpty
 	private String email;
 	
 	
@@ -13,12 +23,17 @@ public class Usuario {
 	public Usuario() {
 	
 	}
-	public Usuario(String username, String password, String email) {
 
+	public Usuario(@NotEmpty String nombre, @NotEmpty String apellidos, @NotEmpty String username,
+			@NotEmpty String password, @NotEmpty String email) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
+
 	//GETTERS Y SETTERS
 	public String getUsername() {
 		return username;
@@ -37,6 +52,30 @@ public class Usuario {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
 	}
 	
 	
